51b7d4fd35775f75914e3c2009c081c8a2a6195b
Evgeny Esaulkov <evgeniy.esaulkov@flatstack.com>
2023-03-12 00:06:30 +0300

    add postgres extensions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 0) do
-  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_buffercache"
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
-
+  enable_extension "sslinfo"
 end
