e97dc71d217d9cc61775726ba120655bcd821382
Evgeny Esaulkov <evgeniy.esaulkov@flatstack.com>
2023-04-23 15:18:18 +0200

    add validations to PlayerStat

--- app/models/player_stat.rb
+++ app/models/player_stat.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class PlayerStat < ApplicationRecord
+  validates :name, presence: true
+  validates :position, presence: true
+  validates :club, presence: true
+  validates :uid, db_uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_093738) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_23_131307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
   enable_extension "pg_stat_statements"
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_093738) do
     t.jsonb "stat"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["uid"], name: "index_player_stats_on_uid", unique: true
   end
 
 end
