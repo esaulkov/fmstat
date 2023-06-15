377d42936a7b11ee66d75df68120c3485d083107
Evgeny Esaulkov <evgeniy.esaulkov@flatstack.com>
2023-06-15 21:10:42 +0200

    add sorting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,23 @@
 
 class HomeController < ApplicationController
   def index
-    @pagy, @stats = pagy(PlayerStat.all)
+    @pagy, @stats = pagy(players)
     render HomeComponent.new(stats: @stats, pagy: @pagy)
+  end
+
+  private
+
+  def sort_params
+    params.permit(:sort)
+  end
+
+  def players
+    sort_column = sort_params[:sort].to_s.strip
+
+    if PlayerStat.sorted_cols.include?(sort_column)
+      PlayerStat.order(Arel.sql("(stat->>'#{sort_column}')::float").desc)
+    else
+      PlayerStat.all
+    end
   end
 end
--- app/models/player_stat.rb
+++ app/models/player_stat.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class PlayerStat < ApplicationRecord
+  SORTED_COLS = %w[apps gls av_rat].freeze
+
   validates :name, presence: true
   validates :position, presence: true
   validates :club, presence: true
   validates :uid, db_uniqueness: true
+
+  def self.sorted_cols
+    SORTED_COLS
+  end
 end
