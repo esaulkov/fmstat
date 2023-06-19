a25d7e44707602a0a408d20b2748f6fd67de433a
Evgeny Esaulkov <evgeniy.esaulkov@flatstack.com>
2023-06-19 11:29:57 +0200

    fix FactoryBot::DuplicateDefinitionError

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require 'factory_bot'
-
-FactoryBot.find_definitions
+require 'rails_helper'
 
 FactoryBot.factories.map(&:name).each do |factory_name|
   describe "The #{factory_name} factory" do
